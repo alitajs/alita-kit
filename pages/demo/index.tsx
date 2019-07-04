02fbfd74aba45a423043968a8c89fd3a6d2cc465
wulili <wu.lili@iwhalecloud.com>
2019-07-04 09:32:53 +0800

    测试

--- pages/demo/index.tsx
+++ pages/demo/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
 import Demo from '../../components/demo';
 
-const Page: FC = () => <Demo text="1221" />;
+const Page: FC = () => <Demo text="122551" />;
 export default Page;
